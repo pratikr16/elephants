72d2c1e20280c83eaaba1ab3ac8e77d8075baab8
Pratik Gupte <pratikgupte16@gmail.com>
2018-03-09 13:36:11 +0530

    more rev stats

--- ele_code/ggplot.opts.r
+++ ele_code/ggplot.opts.r
@@ -6,9 +6,9 @@ g1 = theme_bw()+
   theme(panel.grid = element_blank(),
  legend.position="none", plot.title = element_text(size = 12, face = "bold"))+theme(#strip.text=element_blank(),
 
-  strip.background=element_blank(), panel.spacing.y=unit)+#+ theme(panel.border = element_rect(fill = NA, colour = 1))
+  strip.background=element_blank(), panel.spacing.y=unit)#+#+ theme(panel.border = element_rect(fill = NA, colour = 1))
 
-  theme(axis.ticks.length=unit(-0.1, "cm"), axis.text.x = element_text(margin=unit(rep(0.2,4), "cm")), axis.text.y = element_text(margin=unit(rep(0.2,4), "cm")))
+#  theme(axis.ticks.length=unit(-0.1, "cm"), axis.text.x = element_text(margin=unit(rep(0.2,4), "cm")), axis.text.y = element_text(margin=unit(rep(0.2,4), "cm")))
 
 base_size = 11; base_family = ""
 
