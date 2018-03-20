ef301e57862124bb1416cecf33d3abfc17bef999
Pratik Gupte <pratikgupte16@gmail.com>
2018-03-20 21:50:35 +0530

    add new data and plots

--- ele_code/ele.code003.5weatherdata.r
+++ ele_code/ele.code003.5weatherdata.r
@@ -1,5 +1,6 @@
 ####Loading Skukuza weather station data####
 ####
+source("libs.R")
 
 #'load the skukuza flux tower temp data
 tempdata = read.csv("~/git/elephants/ele_data/skukuza_tower_temp.csv")
@@ -13,3 +14,15 @@ tempdata = tempdata %>% rename(temp.a = X.C)
 
 #'set neg vals to NA
 tempdata = tempdata %>% mutate(temp.a = ifelse(temp.a < 0, NA, temp.a))
+
+####
+#'load eledata
+load("eledata.rdata")
+
+season = ele2 %>% select(time, season2) %>% 
+  group_by(date = round_date(time, "day")) %>% summarise(season2 = first(season2))
+
+tempdata = tempdata %>% mutate(date = round_date(time, 'day')) %>% left_join(season, by = "date")
+
+library(psych)
+describe((tempdata %>% filter(season2 == "dry"))$temp.a)
